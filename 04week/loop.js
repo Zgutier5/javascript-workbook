1a6009edfe7d76c12058d254adb46bee3792485d
Ezequiel Gutierrez <zgutier5@gmail.com>
2018-08-24 14:32:37 -0500

    updated

--- 04week/loop.js
+++ 04week/loop.js
@@ -1,7 +1,45 @@
+//Use a for loop to console.log each item in the array carsInReverse.
+// for...in loop
+const carsInReverse = ['ford', 'dodge', 'chevy']
+    for (i=0; i < carsInReverse.length; i++){ 
+    console.log(carsInReverse)
+    }
 
-const carsInReverse = {
+
+//Create an object called persons with the following data:
+// firstName: "Jane"
+// lastName: "Doe"
+// birthDate: "Jan 5, 1925"
+// gender: "female"
+const people = {
     firstName: "Jane",
-    lastName: "Doe",
+    lastName: "Doe", 
     birthDate: "Jan 5, 1925",
-    gender: "female"}
+    gender: "female"
+}
 
+
+//loop to console.log each key.
+for(people) {
+    console.log(Objects.keys(people))
+}
+
+//console.log the value associated with the key birthDate.
+for(people) {
+    if {
+    firstName = 'Jane'} {
+    console.log(Objects.values[birthDate])
+  }
+
+//Use a for loop to console.log the numbers 1 to 1000.
+// do...while loop
+// Use a do...while loop to console.log the numbers from 1 to 1000.
+
+  for (x = 1; x <= 1000; x ++){
+    console.log(x);
+  }
+
+  do {
+    x += 1;
+    console.log(x);
+  } while(x <= 1000);
